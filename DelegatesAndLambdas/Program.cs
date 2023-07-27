0d2e17b712b1d9b0496c624551022d76f58c8012
lalit.nailwal <lalit.nailwal@petronas.com>
2023-07-27 15:57:58 +0530

    Generic Delgates

--- DelegatesAndLambdas/Program.cs
+++ DelegatesAndLambdas/Program.cs
@@ -1,19 +1,13 @@
 ﻿Console.WriteLine("This example is to illustrate Delegates");
 
-// CalculateAndPrint(25, 30, delegate(int x, int y) { return (x+y);});
+CalculateAndPrint<int>(25, 30, (int x, int y) =>  x * y); 
+CalculateAndPrint("A", "B", (x, y) => x + y);
+CalculateAndPrint(true, false, (x, y) => x && y);
 
-/* Below is a lambda function, A lambda function is just a delegate, 
-a function with less code i:e less letter and digits, here fat arrow is a symbol to write code quickly with less words*/
-CalculateAndPrint(25, 30, (x, y) =>  x + y);
-CalculateAndPrint(25, 30, (x, y) =>  x - y);
-CalculateAndPrint(25, 30, (x, y) =>  x * y); 
-
-/*This function knows how to calculate and print but don't know 
-what operation to perform (The Operation to perform is passed via delegate)*/
-static void CalculateAndPrint(int x, int y, Mathop f)
+static void CalculateAndPrint<T>(T x, T y, Combine<T> f)
 {
-    int result = f(x,y);
+    var result = f(x, y);
     Console.WriteLine(result);
 }
 
-delegate int Mathop(int a, int b); //delegate here is a typed definition for a category of funcitons
+delegate T Combine<T>(T a, T b);
