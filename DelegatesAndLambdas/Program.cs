eff225edf9158f26e064fac97972240ad930e138
lalit.nailwal <lalit.nailwal@petronas.com>
2023-07-27 08:38:17 +0530

    Delegates - Through passing delegate as a function parameter

--- DelegatesAndLambdas/Program.cs
+++ DelegatesAndLambdas/Program.cs
@@ -17,5 +17,15 @@ Console.WriteLine(f(10,20));
 f = Subtract;
 Console.WriteLine(f(10,20));
 
+CalculateAndPrint(25,30,f);
+
+/*This function knows how to calculate and print but don't know 
+what operation to perform (The Operation to perform is passed via delegate)*/
+static void CalculateAndPrint(int x, int y, Mathop f)
+{
+    int result = f(x,y);
+    Console.WriteLine(result);
+}
+
 delegate int Mathop(int a, int b);
 
