167e877337f4b6c43e48c9a89633eb41c1bd571f
lalit.nailwal <lalit.nailwal@petronas.com>
2023-07-27 08:26:44 +0530

    Delegates - Through Simple function assignments

---
+++ DelegatesAndLambdas/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("This example is to illustrate Delegates");
+
+static int Add(int x, int y)
+{
+    int result = x + y;
+    return result;
+}
+
+static int Subtract(int a, int b)
+{
+    int result = a - b;
+    return result;
+}
+
+Mathop f = Add;
+Console.WriteLine(f(10,20));
+f = Subtract;
+Console.WriteLine(f(10,20));
+
+delegate int Mathop(int a, int b);
+
