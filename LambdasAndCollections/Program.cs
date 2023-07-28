a43854992c655907b5812727131f50e56ba487be
lalit.nailwal <lalit.nailwal@petronas.com>
2023-07-28 13:22:26 +0530

    Introduction of Framework provided delegate called Func<>

--- LambdasAndCollections/Program.cs
+++ LambdasAndCollections/Program.cs
@@ -18,7 +18,7 @@ Console.WriteLine(heroesWhoCanFly);
 var result1 = Filter(new string[] {"lalit","mohan","nailwal"}, x => x.StartsWith("l"));
 var result2 = Filter(new int[] {1,2,3,4}, x=> x % 2 == 0);
 
-IEnumerable<T> Filter<T>(IEnumerable<T> heroes, Filter<T> f) //This is a generic filter capable of filtering any type of object for any condition
+IEnumerable<T> Filter<T>(IEnumerable<T> heroes, Func<T, bool> f) //This is a generic filter capable of filtering any type of object for any condition same as like LINQ where
 {
     foreach(var hero in heroes)
     {
@@ -26,7 +26,5 @@ IEnumerable<T> Filter<T>(IEnumerable<T> heroes, Filter<T> f) //This is a generic
              yield return hero;  //yield helps us to get rid of cration of new list thus save on memory footprints
     }
 }
-
-delegate bool Filter<T>(T hero);
 
 record Hero(string FistName, string LastName, string HeroName, bool CanFly);
